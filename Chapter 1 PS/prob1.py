3c57b85330947328ae2864d44b39d21235881bd8
Al Faravi <alfaravi17@gmail.com>
2025-05-29 11:00:49 +0600

    Add Chapter 1 practice files

---
+++ Chapter 1 PS/prob1.py
@@ -0,0 +1,12 @@
+# Write a program to print multiline print code.
+
+print('''
+CHEM 101: Chemistry
+EEE-327: Transmission and Distribution of Electrical Power
+EEE-329: VLSI I 
+EEE-407: Optical Fiber Communication 
+HUM-207: Engineering Economics
+''')
+
+
+
---
+++ module.py
@@ -0,0 +1,6 @@
+import pyjokes
+
+# this is a python sample code
+
+joke = pyjokes.get_joke()
+print(joke)
